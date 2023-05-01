4c57cc8495a471eba39e81dd2a10d03ca4cc8777
555 <Shebelnitskiy@gmail.com>
2023-05-01 20:55:27 +0400

    upgrade class JSONSaver

--- src/json_saver.py
+++ src/json_saver.py
@@ -5,7 +5,7 @@ class JSONSaver:
         self.file_name_vacancy: str = 'JSON_vacancy.json'
         self.file_name_employers: str = 'JSON_employers.json'
 
-    def add_vacancies(self, vacancyes, employers):
+    def add_vacancies(self, vacancyes=None, employers=None):
         """
         Сохраняет информацию о вакансиях в файл JSON.
 
@@ -14,5 +14,7 @@ class JSONSaver:
         """
         with open(self.file_name_vacancy, 'w', encoding='utf-8') as file:
             json.dump(vacancyes,file, ensure_ascii=False, indent=4)
+            print(len(vacancyes))
         with open(self.file_name_employers, 'w', encoding='utf-8') as file:
             json.dump(employers,file, ensure_ascii=False, indent=4)
+            print(len(employers))
