d0843e9670dbfa88a41e61f9848fde8425a383ba
555 <Shebelnitskiy@gmail.com>
2023-05-03 18:15:38 +0400

    upgrade main

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-from src.utils import creating_tables
+from src.utils import creating_tables_check, connect_pg
+from src.dbmanager import DBManager
+
 
 if __name__ == "__main__":
-    creating_tables()
+    host, database, user, password = 'localhost', 'term_papers_5', 'postgres', '1346'
+    creating_tables_check(host, database, user, password)
+    manager = DBManager(host, database, user, password)
+    manager.get_companies_and_vacancies_count()
