c7f2398c3dd9ad3e78053cc4967b14e7d555cbbb
555 <Shebelnitskiy@gmail.com>
2023-05-03 18:15:13 +0400

    adding sql queries

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,3 +1,4 @@
+### создание таблицы с вакансиями
 CREATE TABLE vacancy (
   id VARCHAR(50) PRIMARY KEY,
   job_title VARCHAR(100) NOT NULL,
@@ -6,15 +7,20 @@ CREATE TABLE vacancy (
   currency VARCHAR(10) NOT NULL,
   description TEXT,
   area VARCHAR(100) NOT NULL,
+  id_employer VARCHAR(50),
   employer VARCHAR(100) NOT NULL,
   url VARCHAR(200) NOT NULL,
   date TIMESTAMP NOT NULL
 );
-
+### создание таблицы с компаниями
 CREATE TABLE employers (
-id_vacancy INT PRIMARY KEY,
-id_company INT,
+id_company VARCHAR(50) PRIMARY KEY,
 employer_name VARCHAR(100),
 description TEXT,
 site TEXT
 );
+
+### запрос sql на выгрузку количества вакансий у компании
+SELECT employers.employer_name, COUNT(vacancy.id) as count_vacancies
+FROM vacancy JOIN employers ON vacancy.id_employer = employers.id_company
+GROUP BY employers.employer_name
