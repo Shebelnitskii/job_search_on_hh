318ef89d117125593d2c667cbfc92bc089eede5e
555 <Shebelnitskiy@gmail.com>
2023-05-03 18:11:45 +0400

    upgrade class HeadHunter

--- src/headhunter.py
+++ src/headhunter.py
@@ -1,5 +1,6 @@
 import requests
 
+
 class HeadHunterAPI():
     def __init__(self):
         self.url_vacancies = f"https://api.hh.ru/vacancies"  # URL API HeadHunter
@@ -14,54 +15,57 @@ class HeadHunterAPI():
         page = 0
         vacancies = []
         employers = []
-        vacancy_all_id = []
         while True:
             params = {"per_page": 100, "employer_id": employer_id,  ### id работодателя
-                      #'specialization': 1, 'experience': exp,  ### код отрасли (IT, компьютеры, связь)
+                      # 'specialization': 1, 'experience': exp,  ### код отрасли (IT, компьютеры, связь)
                       'page': page}
             response_vacancy = requests.get(self.url_vacancies, params=params)
-            response_employers = requests.get(f'https://api.hh.ru/employers/{employer_id}')
             if response_vacancy.ok:
-                if response_employers.ok:
-                    data_employer = response_employers.json()
-                    data_vacancy = response_vacancy.json()
-                    vacancies_data = data_vacancy['items']
-                    for vacancy in vacancies_data:
-                        if len(vacancies) >= limit_vacancy:
-                            break
-                        salary_data = vacancy['salary']
-                        try:
-                            if salary_data['to'] == None:  ### Проверка на диапазон зарплаты
-                                salary = {'from': salary_data['from'], 'to': 0, 'currency': salary_data['currency']}
-                            elif salary_data['from'] == None:
-                                salary = {'from': 0, 'to': salary_data['to'], 'currency': salary_data['currency']}
-                            else:
-                                salary = {'from': salary_data['from'], 'to': salary_data['to'],
-                                          'currency': salary_data['currency']}
-                        except:
-                            salary = {'from': 0, 'to': 0,
-                                      'currency': 'RUR'}  ### Если зарплата не указано то в значение фром сохраняется нулевое значение
-                        vacancy = {'id': vacancy['id'],
-                                   'job_title': vacancy['name'],  ### Название вакансии
-                                   'salary_from': salary['from'],
-                                   'salary_to': salary['to'],
-                                   'currency': salary['currency'],
-                                   'description': vacancy['snippet']['requirement'],  ### Описание вакансии
-                                   'area': vacancy['area']['name'],
-                                   'employer': vacancy['employer']['name'],
-                                   'url': vacancy['alternate_url'],  ### url ссылка на вакансию
-                                   'date': vacancy['published_at']}  ### дата публикации вакансии
-                        employer = {'id_vacancy': vacancy['id'],
-                                    'id_company': data_employer['id'],
-                                    'employer_name': data_employer['name'],
-                                    'description': data_employer['description'],
-                                    'site': data_employer['site_url']}
-                        vacancy_all_id.append(vacancy["id"])
-                        employers.append(employer)
-                        vacancies.append(vacancy)
-                    if page >= data_vacancy['pages'] - 1 or len(vacancies) >= limit_vacancy:
+                data_vacancy = response_vacancy.json()
+                vacancies_data = data_vacancy['items']
+                for vacancy in vacancies_data:
+                    if len(vacancies) >= limit_vacancy:
                         break
-                    page += 1
+                    salary_data = vacancy['salary']
+                    try:
+                        if salary_data['to'] == None:  ### Проверка на диапазон зарплаты
+                            salary = {'from': salary_data['from'], 'to': 0, 'currency': salary_data['currency']}
+                        elif salary_data['from'] == None:
+                            salary = {'from': 0, 'to': salary_data['to'], 'currency': salary_data['currency']}
+                        else:
+                            salary = {'from': salary_data['from'], 'to': salary_data['to'],
+                                      'currency': salary_data['currency']}
+                    except (KeyError, TypeError):
+                        salary = {'from': 0, 'to': 0, 'currency': 'RUR'}  ### Если зарплата не указано то в значение фром сохраняется нулевое значение
+                    vacancy = {'id': vacancy['id'],
+                               'job_title': vacancy['name'],  ### Название вакансии
+                               'salary_from': salary['from'],
+                               'salary_to': salary['to'],
+                               'currency': salary['currency'],
+                               'description': vacancy['snippet']['requirement'],  ### Описание вакансии
+                               'area': vacancy['area']['name'],
+                               'id_employer': vacancy['employer']['id'],
+                               'employer': vacancy['employer']['name'],
+                               'url': vacancy['alternate_url'],  ### url ссылка на вакансию
+                               'date': vacancy['published_at']}  ### дата публикации вакансии
+                    if not any(vacancy['id'] == v['id'] for v in vacancies):
+                        vacancies.append(vacancy)
+                if page >= data_vacancy['pages'] - 1 or len(vacancies) >= limit_vacancy:
+                    break
+                page += 1
+
             else:
                 break
+
+
+        response_employers = requests.get(f'https://api.hh.ru/employers/{employer_id}')
+        if response_employers.ok:
+            data_employer = response_employers.json()
+            employer = {'id_company': data_employer['id'],
+                        'employer_name': data_employer['name'],
+                        'description': data_employer['description'],
+                        'site': data_employer['site_url']}
+            if employer not in employers:
+                employers.append(employer)
+
         return vacancies, employers
